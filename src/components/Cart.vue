92b7bdfa2037e089925d4ee56026d03a5f1039df
Aeon Tanvir <aeontanvir@gmail.com>
2020-01-21 23:07:42 +0600

    Single file component

---
+++ src/components/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item">Product - Price</li>
+    <li class="list-group-item">Product - Price</li>
+    <li class="list-group-item">Product - Price</li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/Inventory.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="card" style="width: 18rem;">
+      <img src="https://dummyimage.com/600x400/000/fff" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and content.</p>
+        <a href="#" class="btn btn-primary">Add Product</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
